593719c2dabf561e07d3035d620f8695c7720f93
bap2494 <brian_a_pruitt@homedepot.com>
2025-03-04 16:05:50 -0500

    added htmlnode and tests

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
+from htmlnode import HTMLNode
 from textnode import TextNode, TextType
 
 def main():
     print(TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev"))
+    print(HTMLNode("a", "Click here", [], {"href": "https://www.google.com", "target": "_blank"}))
 
 main()

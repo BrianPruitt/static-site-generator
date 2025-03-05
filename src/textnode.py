d90ab565f186fceec14bc25219ea255f843519d6
bap2494 <brian_a_pruitt@homedepot.com>
2025-03-05 13:59:52 -0500

    added inline markdown and tests

--- src/textnode.py
+++ src/textnode.py
@@ -1,5 +1,7 @@
 from enum import Enum
 
+from htmlnode import LeafNode
+
 class TextType(Enum):
     TEXT = "text"
     BOLD = "bold"
@@ -20,7 +22,30 @@ class TextNode:
             and self.text_type == node_two.text_type 
             and self.url == node_two.url
         )
-        
     
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+    
+def text_node_to_html_node(text_node):
+    if not isinstance(text_node, TextNode):
+        raise ValueError("Invalid input: Expected a TextNode")
+    
+    match text_node.text_type:
+        case TextType.TEXT:
+            return LeafNode(None, text_node.text)
+        case TextType.BOLD:
+            return LeafNode("b", text_node.text)
+        case TextType.ITALIC:
+            return LeafNode("i", text_node.text)
+        case TextType.CODE:
+            return LeafNode("code", text_node.text)
+        case TextType.LINK:
+            if text_node.url is None:
+                return ValueError("Link TextNode must have a URL")
+            return LeafNode("a", text_node.text, {"href": text_node.url})
+        case TextType.IMAGE:
+            if text_node.url is None:
+                return ValueError("Image TextNode must have a URL")
+            return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+        case _:
+            raise ValueError(f"Invalid TextType: {text_node.text_type}")

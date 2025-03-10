0029b017e68602e8a0720a604535e125d8a6f7db
bap2494 <brian_a_pruitt@homedepot.com>
2025-03-10 11:03:40 -0400

    getting pages to work boiiii

---
+++ build.sh
@@ -0,0 +1,2 @@
+REPO_NAME="https://github.com/BrianPruitt/static-site-generator"
+python3 src/main.py "/${REPO_NAME}/"
--- src/main.py
+++ src/main.py
@@ -1,26 +1,30 @@
 import os
 import shutil
+import sys
 
 from copystatic import copy_files_recursive
 from gencontent import generate_page
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_public = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
 
 
-def generate_pages_recursive(content_dir, template_path, public_dir):
+def generate_pages_recursive(content_dir, template_path, public_dir, basepath):
     for root, _, files in os.walk(content_dir):
         for file in files:
             if file.endswith(".md"):
                 from_path = os.path.join(root, file)
                 relative_path = os.path.relpath(from_path, content_dir)
                 dest_path = os.path.join(public_dir, relative_path).replace(".md", ".html")
-                generate_page(from_path, template_path, dest_path)
+                generate_page(from_path, template_path, dest_path, basepath)
 
 
 def main():
+    basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
+
+
     print("Deleting public directory...")
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
@@ -29,7 +33,7 @@ def main():
     copy_files_recursive(dir_path_static, dir_path_public)
 
     print("Generating pages...")
-    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public, basepath)
 
 
 main()

fa56bccad2efdf68cba253312500361a0a421ad3
bap2494 <brian_a_pruitt@homedepot.com>
2025-03-10 11:31:21 -0400

    still trying

--- build.sh
+++ build.sh
@@ -1,2 +1,1 @@
-REPO_NAME="https://github.com/BrianPruitt/static-site-generator"
-python3 src/main.py "/${REPO_NAME}/"
+python3 src/main.py "/https://github.com/BrianPruitt/static-site-generator/"
--- src/gencontent.py
+++ src/gencontent.py
@@ -32,6 +32,7 @@ def generate_page(from_path, template_path, dest_path, basepath):
     template = template.replace("{{ Content }}", html)
     template = template.replace('href="/', f'href="{basepath}')
     template = template.replace('src="/', f'src="{basepath}')
+    template = template.replace('url("/', f'url("{basepath}')
 
     dest_dir_path = os.path.dirname(dest_path)
     if dest_dir_path != "":
